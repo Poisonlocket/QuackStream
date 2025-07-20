dc4302f6c8f9ecd7c27216282b0a267f805ebfc1
Poisonlocket <loribono@gmx.ch>
2025-07-21 00:14:42 +0200

    stashing changes, irrelevant, this branch is only for testing

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
 
 app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Quack Successful"}
+
+@app.get("/418")
+async def teapot():
+    return {"message": "Congrats, you found the teapot"}
+
+@app.post("/webhook")
+async def websocket(request: Request):
+    payload = await request.json()
+    commit_description = payload["head_commit"]["message"]
+    author_name = payload["head_commit"]["author"]["name"]
+    author_email = payload["head_commit"]["author"]["email"]
+    repository_name = payload["repository"]["name"]
+
+    print("Commit description:", commit_description)
+    print("Author:", author_name)
+    print("Author email:", author_email)
+    print("Repository name:", repository_name)
+
