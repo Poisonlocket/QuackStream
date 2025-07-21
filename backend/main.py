edeb5446ae463f04a5b12968f81c0b1fe2cf356d
Poisonlocket <loribono@gmx.ch>
2025-07-21 14:36:37 +0200

    added requirements file

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,7 @@
+import random
+import uuid
+from xml.etree.ElementTree import tostring
+
 from fastapi import FastAPI, Request, WebSocket
 from util.util import serialize_data
 
@@ -12,7 +16,6 @@ async def root():
 @app.get("/418")
 async def teapot():
     return {"message": "Congrats, you found the teapot"}
-
 @app.post("/webhook")
 async def webhook(request: Request):
     global ws_client
@@ -52,11 +55,17 @@ async def websocket_endpoint(websocket: WebSocket):
 @app.get("/test_websocket")
 async def test_websocket():
     global ws_client
-    prepared_json_data = {"author_name": "Poisonlocket", "commit_description": "Quack", "repository_name": "Quackstream", "avatar_url": "https://avatars.githubusercontent.com/u/128643203?v=4"}
+
     if ws_client:
         try:
+
+            prepared_json_data = {"author_name": "Poisonlocket", "commit_description": "QuackIt is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+                                  "repository_name": "Quackstream",
+                                  "avatar_url": "https://avatars.githubusercontent.com/u/128643203?v=4", "id": str(uuid.uuid4())}
+
             await ws_client.send_json(prepared_json_data)
             print("Forwarded webhook to WebSocket client")
+
         except Exception as e:
             print("Error sending to WebSocket client:", e)
             ws_client = None
