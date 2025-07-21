2016d685d62d512b234262293f8a08bbc3b06595
Poisonlocket <loribono@gmx.ch>
2025-07-21 11:19:54 +0200

    added avatar and profile url to the data serialization function,  added a testing endpoint to mock data to the frontend

--- backend/main.py
+++ backend/main.py
@@ -48,3 +48,15 @@ async def websocket_endpoint(websocket: WebSocket):
     finally:
         ws_client = None
         print("WebSocket client disconnected")
+
+@app.get("/test_websocket")
+async def test_websocket():
+    global ws_client
+    prepared_json_data = {"author_name": "Poisonlocket", "commit_description": "Quack", "repository_name": "Quackstream", "avatar_url": "https://avatars.githubusercontent.com/u/128643203?v=4"}
+    if ws_client:
+        try:
+            await ws_client.send_json(prepared_json_data)
+            print("Forwarded webhook to WebSocket client")
+        except Exception as e:
+            print("Error sending to WebSocket client:", e)
+            ws_client = None
--- backend/util/util.py
+++ backend/util/util.py
@@ -4,5 +4,7 @@ def serialize_data(payload: dict) -> dict:
     commit_description = payload["head_commit"]["message"]
     author_name = payload["head_commit"]["author"]["name"]
     repository_name = payload["repository"]["name"]
+    avatar_url = payload["owner"]["avatar_url"]
+    profile_url = payload["owner"]["url"]
 
-    return {"author_name": author_name, "commit_description": commit_description, "repository_name": repository_name}
+    return {"author_name": author_name, "commit_description": commit_description, "repository_name": repository_name, "avatar_url": avatar_url, "profile_url": profile_url}
